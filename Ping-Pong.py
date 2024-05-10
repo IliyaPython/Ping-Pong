5d903a94915f669aa1c05f54d827ac809ad1f56a
IliyaPython <riseofkingdom5and6@gmail.com>
2024-05-10 16:58:43 +1000

    v 0.2
    Class added

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -6,7 +6,16 @@ window = display.set_mode((700, 256))
 display.set_caption('Ping-Pong')
 
 background = transform.scale(image.load('ping-pong.jpg'), (700, 256))
-
+class GameSprite(sprite.Sprite):
+    def __init__(self, sprite_image, cor_x, cor_y, w, h, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_image), (w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x = cor_x
+        self.rect.y = cor_y
+        self.speed = speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 procces = True
 clock = time.Clock()
 
@@ -15,6 +24,6 @@ while procces:
         if e.type == QUIT:
             procces = False
     window.blit(background, (0,0))
-    
+
     clock.tick(60)
     display.update()
